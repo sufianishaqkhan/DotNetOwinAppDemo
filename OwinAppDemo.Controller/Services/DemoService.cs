59e59895c6f37c2f3012899bcd521cc9bc3a3137
Sufian Ishaq Khan <sufianishaqkhan@gmail.com>
2023-06-03 19:56:10 +0500

    Owin app boiler plate code added.

---
+++ OwinAppDemo.Controller/Services/DemoService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OwinAppDemo.Controller.Services
+{
+    public class DemoService
+    {
+        public DemoService()
+        {
+
+        }
+
+        public List<string> GetAllValues()
+        {
+            List<string> valuesList = new List<string>();
+
+            valuesList.Add("service.value1");
+            valuesList.Add("service.value2");
+            valuesList.Add("service.value3");
+
+            return valuesList;
+        }
+    }
+}
