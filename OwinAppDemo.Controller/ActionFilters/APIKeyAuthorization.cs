457aa681980308a798efc06804d9c74012bc1356
Sufian Ishaq Khan <sufianishaqkhan@gmail.com>
2023-06-03 19:57:01 +0500

    Demo controller and api auth added.

---
+++ OwinAppDemo.Controller/ActionFilters/APIKeyAuthorization.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace OwinAppDemo.Controller.ActionFilters
+{
+    public class APIKeyAuthorization : AuthorizeAttribute
+    {
+        private const string ApiKeyHeaderName = "ApiKey";
+
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            IEnumerable<string> apiKeyHeaderValues = null;
+
+            if (actionContext.Request.Headers.TryGetValues(ApiKeyHeaderName, out apiKeyHeaderValues))
+            {
+                bool isValidAPIKey = false;
+                IEnumerable<string> lsHeaders;
+
+                var checkApiKeyExists = actionContext.Request.Headers.TryGetValues(ApiKeyHeaderName, out lsHeaders);
+
+                if (checkApiKeyExists)
+                {
+                    if (lsHeaders.FirstOrDefault().Equals(ConfigurationManager.AppSettings["ApiKey"]))
+                    {
+                        isValidAPIKey = true;
+                    }
+                }
+
+                if (!isValidAPIKey)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            return base.IsAuthorized(actionContext);
+        }
+    }
+}
---
+++ OwinAppDemo.Controller/Controllers/DemoController.cs
@@ -0,0 +1,70 @@
+﻿using OwinAppDemo.Controller.ActionFilters;
+using OwinAppDemo.Controller.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OwinAppDemo.Controller.Controllers
+{
+    [RoutePrefix("api/Demo")]
+    [APIKeyAuthorization]
+    public class DemoController : ApiController
+    {
+        /// <summary>
+        /// Get all values
+        /// </summary>
+        /// <returns>List of values</returns>
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            var list = new string[] { "value1", "value2" };
+
+            return Ok(list);
+        }
+
+        /// <summary>
+        /// Get value by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>List of values</returns>
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            return Ok("value" + id);
+        }
+
+        /// <summary>
+        /// Get all values without auth key
+        /// </summary>
+        /// <returns>List of values</returns>
+        [HttpGet]
+        [Route("GetAnonymous")]
+        [AllowAnonymous]
+        public async Task<IHttpActionResult> GetAnonymous()
+        {
+            var list = new string[] { "Anonymous_value1", "Anonymous_value2" };
+
+            return Ok(list);
+        }
+
+        /// <summary>
+        /// Get all values from service
+        /// </summary>
+        /// <returns>List of values</returns>
+        [HttpGet]
+        [Route("GetAllFromService")]
+        public async Task<IHttpActionResult> GetAllFromService()
+        {
+            DemoService demoService = new DemoService();
+            var result = await Task.FromResult(demoService.GetAllValues());
+
+            return Ok(result);
+        }
+    }
+}
